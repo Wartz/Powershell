112da7bd9d6e41e7ac6d19cc096ec3fe714fea50
Joe Schlimmer <joseph.schlimmer@gmail.com>
2024-01-13 09:05:10 -0500

    Update Install-WindowsAdminCenter.ps1

--- WindowsAdminCenter/Install-WindowsAdminCenter.ps1
+++ WindowsAdminCenter/Install-WindowsAdminCenter.ps1
@@ -4,4 +4,5 @@ $file = "wac2311.msi"
 Invoke-WebRequest -uri $uri -OutFile $file
 
 # Install WAC with self signed cert and default port
+# Set SME_PORT=443 for self-signed https
 msiexec /i .\wac2311.msi /qn /L*v log.txt SME_PORT=6516 SSL_CERTIFICATE_OPTION=generate
