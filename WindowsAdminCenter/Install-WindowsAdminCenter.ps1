a06f3cc42ba06133563c057b3e800bc545c4c8db
Joe Schlimmer <joseph.schlimmer@gmail.com>
2024-01-25 11:35:54 -0500

    Update Install-WindowsAdminCenter.ps1

--- WindowsAdminCenter/Install-WindowsAdminCenter.ps1
+++ WindowsAdminCenter/Install-WindowsAdminCenter.ps1
@@ -1,7 +1,11 @@
+$ProgressPreference = 'SilentlyContinue'
+
 $uri = "https://download.microsoft.com/download/1/0/5/1059800B-F375-451C-B37E-758FFC7C8C8B/WindowsAdminCenter2311.msi"
-$file = "wac2311.msi"
+$file = "WindowsAdminCenter2311.msi"
 
 Invoke-WebRequest -uri $uri -OutFile $file
 
 # Install WAC with self signed cert and default port
 msiexec /i .\wac2311.msi /qn /L*v log.txt SME_PORT=6516 SSL_CERTIFICATE_OPTION=generate
+
+$ProgressPreference = 'Continue'
