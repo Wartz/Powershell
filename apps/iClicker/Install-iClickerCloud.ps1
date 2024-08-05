5c0e28ef5eff308f2e04a5dc212def4c39c61996
Joe Schlimmer <joseph.schlimmer@gmail.com>
2024-08-05 15:10:42 -0400

    Update Install-iClickerCloud.ps1

--- apps/iClicker/Install-iClickerCloud.ps1
+++ apps/iClicker/Install-iClickerCloud.ps1
@@ -21,38 +21,50 @@ function Uninstall-iClickerCloud {
     Start-Process -FilePath "msiexec.exe" -ArgumentList "/x `"$msi_tagID`"", "/qn", "/l*v `"$logpath`"" -Wait -NoNewWindow
 }
 
+function Set-IClickerCloudRegistry {
+    [CmdletBinding()]
+    param(
+        [string]$KeyPath = "HKCU:\Software\iClicker Cloud",
+        [hashtable]$ValuePairs = @{
+            "installed" = 1
+            "optional_software_update" = 1
+        }
+    )
+
+    process {
+        try {
+            # Create the key if it doesn't exist
+            if (!(Test-Path $KeyPath)) {
+                New-Item -Path $KeyPath -Force | Out-Null
+            }
+
+            # Set the values as REG_DWORD
+            foreach ($valueName in $ValuePairs.Keys) {
+                New-ItemProperty -Path $KeyPath -Name $valueName -Value $ValuePairs[$valueName] -PropertyType DWord -Force | Out-Null
+            }
+
+            # Verify the key and values were created
+            if (Test-Path $KeyPath) {
+                foreach ($valueName in $ValuePairs.Keys) {
+                    $value = Get-ItemPropertyValue -Path $KeyPath -Name $valueName
+                    Write-Output "Value '$valueName' created successfully. Value: $value"
+                }
+            } else {
+                throw "Failed to create the registry key."
+            }
+        }
+        catch {
+            Write-Error "An error occurred: $_"
+        }
+    }
+}
+
 switch ($Action) {
     "install" { 
         Install-iClickerCloud
+        Set-IClickerCloudRegistry
      }
      "uninstall" {
         Uninstall-iClickerCloud
      }
-}
-
-# Add HKCU registry key to avoid the msi reconfig popup window on first launch
-$keyPath = "HKCU:\Software\iClicker Cloud"
-$valuePairs = @{
-    "installed" = 1
-    "optional_software_update" = 1
-}
-
-# Create the key if it doesn't exist
-if (!(Test-Path $keyPath)) {
-    New-Item -Path $keyPath -Force | Out-Null
-}
-
-# Set the values as REG_DWORD
-foreach ($valueName in $valuePairs.Keys) {
-    New-ItemProperty -Path $keyPath -Name $valueName -Value $valuePairs[$valueName] -PropertyType DWord -Force
-}
-
-# Verify the key and values were created
-if (Test-Path $keyPath) {
-    foreach ($valueName in $valuePairs.Keys) {
-        $value = Get-ItemPropertyValue -Path $keyPath -Name $valueName
-        Write-Output "Value '$valueName' created successfully. Value: $value"
-    }
-} else {
-    Write-Output "Failed to create the registry key."
 }
