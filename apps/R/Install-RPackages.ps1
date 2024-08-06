b63a82c7b04b805e766e77a0fa42d6411dcb71aa
Joe Schlimmer <joseph.schlimmer@gmail.com>
2024-08-06 10:46:00 -0400

    Fixed Packages parameter to split package list into individual packages

--- apps/R/Install-RPackages.ps1
+++ apps/R/Install-RPackages.ps1
@@ -1,10 +1,10 @@
 <#
 .SYNOPSIS
-Installs a set of packages for the R programming lanaguage
+Installs a set of packages for the R programming language
 .DESCRIPTION
-Installs a set of packages for the R programming lanaguage
+Installs a set of packages for the R programming language
 .PARAMETER Packages
-Takes a list of 1 or more double quoted package names and install them in order.
+Takes a single string of package names, separated by commas, wrapped in double quotes.
 .EXAMPLE
 Install-RPackages.ps1 -Packages "tidyverse, infer, broom, skimr, stats, statsr, openintro, resample, knitr"
 .NOTES
@@ -14,8 +14,12 @@ Related links or references.
 #>
 param(
     [Parameter(Mandatory=$true)]
-    [string[]]$Packages
+    [string]$Packages
 )
+
+# Split the Packages string into an array
+$PackageArray = $Packages -split ',' | ForEach-Object { $_.Trim() }
+
 # Base path for R installation
 $rBasePath = "C:\Program Files\R"
 # Get the latest R version installed
@@ -39,11 +43,12 @@ $systemLibPath = Join-Path -Path $rBasePath -ChildPath "$rVersion\library"
 Write-Debug "Using R version: $rVersion"
 Write-Debug "Rscript path: $rscriptPath"
 Write-Debug "System-wide library path: $systemLibPath"
-foreach ($package in $Packages) {
+
+foreach ($package in $PackageArray) {
     Write-Output "Installing package: $package"
    
     # Use single quotes for R code to avoid escaping issues
-    $rCode = "-e `"install.packages(c('$package'), repos='https://cran.rstudio.com/')`""
+    $rCode = "-e `"install.packages('$package', repos='https://cran.rstudio.com/')`""
     # Call Rscript.exe to execute the R code
     $process = Start-Process -FilePath $rscriptPath -ArgumentList "$rCode" -NoNewWindow -PassThru -Wait
     if ($process.ExitCode -eq 0) {
