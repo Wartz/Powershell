e2c6e6a5e0ca5b6bfa7e9e58427ccf9c23c742fd
Joe Schlimmer <joseph.schlimmer@gmail.com>
2026-04-29 11:24:59 -0400

    Enhance Install-iClickerCloud.ps1 with documentation and logging
    Added synopsis, description, and example usage to the script header. Improved logging setup and added output message during installation.

--- apps/iClicker/Install-iClickerCloud.ps1
+++ apps/iClicker/Install-iClickerCloud.ps1
@@ -1,4 +1,21 @@
-# Powershell.exe -ExecutionPolicy ByPass -File Install-iClickerCloud.ps1 -Action "install" OR "uninstall"
+   <#
+   .SYNOPSIS
+   Installs iClicker cloud desktop from msi in package folder
+   
+   .DESCRIPTION
+   Installs iClicker cloud desktop from msi in package folder
+   
+   .EXAMPLE
+   Powershell.exe -ExecutionPolicy ByPass -File Install-iClickerCloud.ps1 -Action "install"
+   Powershell.exe -ExecutionPolicy ByPass -File Install-iClickerCloud.ps1 -Action "uninstall"
+   
+   .NOTES
+   iClicker Cloud requires the creation of a registry key at `HKLM:\Software\iClicker Cloud`
+   with two values: `installed` and `optional_software_update`, both set to `1`. 
+   This registry key is used by a separate script (Set-iClickerCloudRegistryKey.ps1) to copy the key to the HKEY_CURRENT_USER hive.
+   #>
+
+# 
 [CmdletBinding()]
 param (
     [Parameter(Mandatory=$true)]
@@ -6,14 +23,23 @@ param (
     [string]$Action
 )
 
-$logpath = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\iClicker-Cloud-installer-verbose.log"
+$log = "iClicker-Cloud-installer-verbose.log"
+$logDirectory = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\"
+
+if (-not (Test-Path -Path $logDirectory)) {
+    New-Item -Path $logDirectory -ItemType Directory -Force
+}
+
+$logpath = "$logDirectory\$log"
+
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 Set-Location -Path $scriptDir
 $msiPath = Get-ChildItem ".\" -recurse | Where-Object {$_.extension -eq ".msi"} | ForEach-Object { $_.FullName }
 
 
 function Install-iClickerCloud {
-    Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$msiPath`"", "/qn", "/l*v `"$logpath`"" -Wait -NoNewWindow
+    Write-Output "Installing iClicker Cloud"
+    Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$msiPath`"",  "/qn", "/l*v `"$logpath`"" -Wait -NoNewWindow
 }
 
 function Uninstall-iClickerCloud {
